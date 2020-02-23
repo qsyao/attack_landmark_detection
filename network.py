42bce142a72c4fe6535d4a92679de1f2f5c6d87f
qingsongyao <qingsongyao@compute-t01.future.cn>
2020-02-23 15:44:26 +0800

    deeper

--- network.py
+++ network.py
@@ -10,6 +10,7 @@ class UNet(nn.Module):
         self.bilinear = bilinear
 
         self.inc = DoubleConv(n_channels, 64)
+        self.down0 = Down(64, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
@@ -18,20 +19,23 @@ class UNet(nn.Module):
         self.up2 = Up(512, 128, bilinear)
         self.up3 = Up(256, 64, bilinear)
         self.up4 = Up(128, 64, bilinear)
+        self.up5 = Up(128, 64, bilinear)
         self.outc = OutConv_Sigmoid(64, n_classes)
         self.outc_regression_x = OutConv(64, n_classes)
         self.outc_regression_y = OutConv(64, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
-        x2 = self.down1(x1)
+        x0 = self.down0(x1)
+        x2 = self.down1(x0)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x = self.up4(x, x0)
+        x = self.up5(x, x1)
         logits = self.outc(x)
         regression_x = self.outc_regression_x(x)
         regression_y = self.outc_regression_y(x)
